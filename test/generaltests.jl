7804b87d17de9ebed789e7b0efa6a82cd597401a
fernandopenaranda <49433311+fernandopenaranda@users.noreply.github.com>
2025-02-25 10:47:35 +0100

    Update generaltests.jl
    Added: model with complex hoppings, motivated by the conversation in #3.

--- test/generaltests.jl
+++ test/generaltests.jl
@@ -1,5 +1,9 @@
 using Quantica
 
+
+## 1) GRAPHENE MONOLAYER
+
+
 h = Quantica.HamiltonianPresets.graphene()
 # Configuration
 c = configuration(
@@ -11,8 +15,26 @@ c = configuration(
     angles = (0., 0.)
     )
 # dos 
-s = dos(c, num_points = 10, num_moments = 10, num_random = 10, num_disorder = 1)
-h5gen(h, c, s, filename = "testfile.h5")
+sdos = dos(c, num_points = 10, num_moments = 10, num_random = 10, num_disorder = 1)
+h5gen(h, c, sdos, filename = "testfile.h5")
 # Optical conductivity struct
-s = conductivity_optical(c,  num_points = 10,  num_moments =10,  num_random = 1,  num_disorder = 1, direction = "yy", temperature = 0.0)
-h5gen(h, c, s, filename = "testfile.h5")
+soptcond = conductivity_optical(c,  num_points = 10,  num_moments =10,  num_random = 1,  num_disorder = 1, direction = "yy", temperature = 0.0)
+h5gen(h, c, soptcond, filename = "testfile.h5")
+
+## 2) KANE-MELE MODEL
+"""
+(ref:https://github.com/pablosanjose/Quantica.jl/blob/master/docs/src/tutorial/hamiltonians.md)
+ same configuration files `c` as in (1)
+"""
+function kane_mele_ham()
+    SOC(dr) = 0.05 * ifelse(iseven(round(Int, atan(dr[2], dr[1])/(pi/3))), im, -im)
+    model = hopping(1, range = neighbors(1)) + 
+            hopping((r, dr) ->  SOC(dr); sublats = :A => :A, range = neighbors(2)) +
+            hopping((r, dr) -> -SOC(dr); sublats = :B => :B, range = neighbors(2))
+    return LatticePresets.honeycomb() |> model
+end
+
+h5gen(kane_mele_ham(), c, sdos, filename = "testfile.h5")
+h5gen(kane_mele_ham(), c, soptcond, filename = "testfile.h5")
+
+
